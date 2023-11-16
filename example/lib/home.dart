8dc24547ded67dbbe7c7807aa4a34317ef79fdc3
jingluoguo <55196987+jingluoguo@users.noreply.github.com>
2023-11-16 13:55:34 +0800

    测试渲染图片

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:arcore_flutter_plugin_example/screens/augmented_faces.dart';
 import 'package:arcore_flutter_plugin_example/screens/augmented_images.dart';
 import 'package:arcore_flutter_plugin_example/screens/image_object.dart';
 import 'package:arcore_flutter_plugin_example/screens/matri_3d.dart';
 import 'package:arcore_flutter_plugin_example/screens/multiple_augmented_images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:widgets_to_image/widgets_to_image.dart';
 import 'screens/hello_world.dart';
 import 'screens/custom_object.dart';
 import 'screens/runtime_materials.dart';
@@ -12,98 +17,132 @@ import 'screens/assets_object.dart';
 import 'screens/auto_detect_plane.dart';
 import 'screens/remote_object.dart';
 
+import 'dart:ui' as ui;
+
 class HomeScreen extends StatelessWidget {
+
+  final WidgetsToImageController _controller = WidgetsToImageController();
+
+  // final GlobalKey _globalKey = GlobalKey();
+  //
+  // Future<Uint8List> widgetToImage() async {
+  //   Completer<Uint8List> completer = Completer();
+  //
+  //   RenderRepaintBoundary render = _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+  //   ui.Image image = await render.toImage(pixelRatio: 20);
+  //   ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+  //   completer.complete(byteData?.buffer.asUint8List());
+  //
+  //   return completer.future;
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ArCore Demo'),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => HelloWorld()));
-            },
-            title: Text("Hello World"),
+      body: Stack(
+        children: [
+          WidgetsToImage(
+            controller: _controller,
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+              child: Text("xxxxxx这里是测试数据", style: TextStyle(color: Colors.red), textAlign: TextAlign.center),
+            ),
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => ImageObjectScreen()));
-            },
-            title: Text("Image"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AugmentedPage()));
-            },
-            title: Text("AugmentedPage"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => MultipleAugmentedImagesPage()));
-            },
-            title: Text("Multiple augmented images"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => CustomObject()));
-            },
-            title: Text("Custom Anchored Object with onTap"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => RuntimeMaterials()));
-            },
-            title: Text("Change Materials Property in runtime"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ObjectWithTextureAndRotation()));
-            },
-            title: Text("Custom object with texture and rotation listener "),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AutoDetectPlane()));
-            },
-            title: Text("Plane detect handler"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => Matrix3DRenderingPage()));
-            },
-            title: Text("3D Matrix"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AssetsObject()));
-            },
-            title: Text("Custom sfb object"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => RemoteObject()));
-            },
-            title: Text("Remote object"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => AugmentedFacesScreen()));
-            },
-            title: Text("Augmented Faces"),
+          ListView(
+            children: <Widget>[
+              ListTile(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => HelloWorld()));
+                },
+                title: Text("Hello World"),
+              ),
+              ListTile(
+                onTap: () async {
+
+                  // var datas = await widgetToImage();
+                  var data = await _controller.capture();
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ImageObjectScreen(bytes: data!)));
+                },
+                title: Text("Image"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => AugmentedPage()));
+                },
+                title: Text("AugmentedPage"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => MultipleAugmentedImagesPage()));
+                },
+                title: Text("Multiple augmented images"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => CustomObject()));
+                },
+                title: Text("Custom Anchored Object with onTap"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => RuntimeMaterials()));
+                },
+                title: Text("Change Materials Property in runtime"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => ObjectWithTextureAndRotation()));
+                },
+                title: Text("Custom object with texture and rotation listener "),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => AutoDetectPlane()));
+                },
+                title: Text("Plane detect handler"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => Matrix3DRenderingPage()));
+                },
+                title: Text("3D Matrix"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => AssetsObject()));
+                },
+                title: Text("Custom sfb object"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => RemoteObject()));
+                },
+                title: Text("Remote object"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => AugmentedFacesScreen()));
+                },
+                title: Text("Augmented Faces"),
+              ),
+            ],
           ),
         ],
       ),
--- example/lib/screens/image_object.dart
+++ example/lib/screens/image_object.dart
@@ -4,6 +4,8 @@ import 'package:flutter/services.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ImageObjectScreen extends StatefulWidget {
+  final Uint8List bytes;
+  const ImageObjectScreen({Key? key, required this.bytes}) : super(key: key);
   @override
   _ImageObjectScreenState createState() => _ImageObjectScreenState();
 }
@@ -18,9 +20,29 @@ class _ImageObjectScreenState extends State<ImageObjectScreen> {
         appBar: AppBar(
           title: const Text('Image object'),
         ),
-        body: ArCoreView(
-          onArCoreViewCreated: _onArCoreViewCreated,
-          enableTapRecognizer: true,
+        body: Stack(
+          children: [
+            ArCoreView(
+              onArCoreViewCreated: _onArCoreViewCreated,
+              enableTapRecognizer: true,
+              planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
+              customPlaneTexturePath: "assets/triangle.png",
+              debug: true,
+            ),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: GestureDetector(
+                  onTap: () async {
+                    arCoreController?.snapshot();
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.red,
+                  ),
+                )),
+          ],
         ),
       ),
     );
@@ -31,17 +53,22 @@ class _ImageObjectScreenState extends State<ImageObjectScreen> {
     arCoreController?.onPlaneTap = _handleOnPlaneTap;
   }
 
+  ArCoreNode? _now;
+
   Future _addImage(ArCoreHitTestResult hit) async {
-    final bytes =
-        (await rootBundle.load('assets/earth.jpg')).buffer.asUint8List();
+      await arCoreController?.removeNode(nodeName: "xxxx");
+      _now = null;
+      _now = ArCoreNode(
+        name: "xxxx",
+        image: ArCoreImage(bytes: widget.bytes, width: 120, height: 120),
+        position: hit.pose.translation + vector.Vector3(0.0, 0.0, 0.0),
+        rotation: hit.pose.rotation + vector.Vector4(0.0, 0.0, 0.0, 0.0),
+      );
 
-    final earth = ArCoreNode(
-      image: ArCoreImage(bytes: bytes, width: 500, height: 500),
-      position: hit.pose.translation + vector.Vector3(0.0, 0.0, 0.0),
-      rotation: hit.pose.rotation + vector.Vector4(0.0, 0.0, 0.0, 0.0),
-    );
+      arCoreController?.addArCoreNodeWithAnchor(_now!);
 
-    arCoreController?.addArCoreNodeWithAnchor(earth);
+      /// 显示或隐藏平面点
+      await arCoreController?.togglePlaneRenderer();
   }
 
   void _handleOnPlaneTap(List<ArCoreHitTestResult> hits) {
